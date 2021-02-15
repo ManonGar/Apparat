d72880b5264344f31f9d50cc03926715d80a128b
Laure Curallucci de Peretti <laurecdp@gmail.com>
2021-02-15 15:32:30 +0100

    create controllers bookings + costumes and routes

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -1,13 +1,28 @@
 class CostumesController < ApplicationController
   def index
+    @costumes = Costume.all
   end
 
   def show
+    @costume = Costume.find(params[:id])
   end
 
   def new
+    @costume = Costume.new
   end
 
   def create
+    @costume = Costume.new(costume_params[:id])
+    if @costume.save
+      redirect_to costume_path(@costume)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def costume_params
+    params.require(:costume).permit(:name, :year, :description, :price, :user_id)
   end
 end
