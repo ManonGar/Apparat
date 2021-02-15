2eeb80789aab17a1b43d1852559962b9a3654dda
Sami Ziani <zianisami0@gmail.com>
2021-02-15 23:01:17 +0100

    ongoing seed for Costumes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'json'
 require 'open-uri'
+require 'nokogiri'
+require 'faker'
 
 Costume.destroy_all
+puts 'Cleaning database'
+sleep(2)
 
-url = 'https://www.costumesdepoque.net/catalogue-costumes-civils?page=2'
-url_opening = open(url).read
-user = JSON.parse(url_opening)
+url = 'https://www.costumesdepoque.net/catalogue-costumes-civils'
+html_file = open(url).read
+html_doc = Nokogiri::HTML(html_file)
 
+html_doc.css('#TPASection_jsrook8y > div > div > div > div > section > div > ul > li').first(3).each do |element|
+  costume = Costume.new(name: Faker::Coffee.blend_name, description: element.search('._3RqKm > h3').text, price: element.search('._23ArP').text.to_i, photo: element.search("div[style*='background-image']")[0].to_s[/(https?:\/\/.*\.(?:png|jpg))/i])
+  # puts element.search('._23ArP').text.to_i
+  # puts element.search('._3RqKm > h3').text
+  # p element.search("div[style*='background-image']")[0].to_s[/(https?:\/\/.*\.(?:png|jpg))/i]
+  costume.save
+end
 
+puts Costume.all
+puts 'Finished'
+
+# puts html_doc.css('#TPASection_jsrook8y > div > div > div > div > section > div > ul > li').first
