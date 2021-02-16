7b15e09d4a5d019dd9bff955204177eec3f6bf59
Manon Garnier <manon.garnier.lehy@gmail.com>
2021-02-16 12:57:01 +0100

    pundit ok

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -1,26 +1,50 @@
 class CostumesController < ApplicationController
+  before_action :set_costume, only: [:show, :update, :destroy, :edit]
+
   def index
-    @costumes = Costume.all
+    @costumes = policy_scope(Costume).order(created_at: :desc)
   end
 
   def new
     @costume = Costume.new
+    authorize @costume
   end
 
   def show
-    @costume = Costume.find(params[:id])
+  end
+
+  def edit
   end
 
   def create
     @costume = Costume.new(costume_params)
+    authorize @costume
     if @costume.save
       redirect_to costume_path(@costume)
     else
-      render :new
+      render :show
     end
   end
 
+  def update
+    if @costume.update(costumes_params)
+      redirect_to costume_path(@costume), notice: "Costume was updated successfully!"
+    else
+      render :show
+    end
+  end
+
+  def destroy
+    @costume.destroy
+    redirect_to costumes_path
+  end
+
   private
+
+  def set_costume
+    @costume = Costume.find(params[:id])
+    authorize @costume
+  end
 
   def costume_params
     params.require(:costume).permit(:name, :year, :description, :price, :user_id)
