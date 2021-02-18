4f834422eaadac0763c5496d00c807bcc3cab5d4
Manon Garnier <manon.garnier.lehy@gmail.com>
2021-02-18 17:15:47 +0100

    flatpicker range and bookings

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,9 +1,27 @@
 import flatpickr from "flatpickr";
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
-const initFlatpickr = () => { 
- flatpickr(".datepicker", {
-   
- });
-};
+const initFlatpickr = () => {
+  const bookings = document.querySelector(".booking");
+  if (bookings) {
+      const disableDateArray = [];
+
+      JSON.parse(bookings.dataset.bookings).forEach((booking) => {
+        console.log(booking.beginning_date);
+        const disableDateObject = {};
+        disableDateObject["from"] = booking.beginning_date;
+        disableDateObject["to"] = booking.ending_date;
+        console.log(disableDateObject);
+        disableDateArray.push(disableDateObject);
+    });
+
+     flatpickr("#range_start", {
+        altInput: true,
+        plugins: [new rangePlugin({ input: "#range_end"})],
+         disable: disableDateArray
+     });
+  }
+
+}
 
 export { initFlatpickr };
