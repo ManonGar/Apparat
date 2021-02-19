1ccd7fd72fe2419157cefbbabd69453b46bf252b
Laure Curallucci de Peretti <laurecdp@gmail.com>
2021-02-19 14:29:32 +0100

    dashboard avec notifs + card

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class DashboardController < ApplicationController
   def dashboard
+    @sum = 0
+    current_user.costumes.each do |costume|
+      @sum += costume.bookings.length
+    end
   end
 end
